d0e1e12093fcf11cb531b54e57ba4903f195344b
Roy Prigat <rp2719@columbia.edu>
2018-07-23 19:05:42 +0300

    committing changes Mon Jul 23 19:05:41 IDT 2018

--- commit.sh
+++ commit.sh
@@ -13,4 +13,4 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+git push heroku master
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,16 @@
 window.onscroll = () => {
-    const nav = document.querySelector('.navbar');
-    if (this.scrollY >= 150) nav.classList.add('scroll');
-    else nav.classList.remove('scroll');
+  const nav = document.querySelector(".navbar");
+  if (this.scrollY >= 150) nav.classList.add("scroll");
+  else nav.classList.remove("scroll");
 };
 
-function handleEmailSubmit() {
-    confirm("Thank you, your message has been sent.");
+$(".contact-form").on("submit", function(e) {
+    e.preventDefault();
+    $('#email-submit').modal();
+});
+
+function sendForm() {
+    $(".contact-form").off('submit');
+    $(".contact-form").submit();
+    $(".contact-form").reset();
 }
